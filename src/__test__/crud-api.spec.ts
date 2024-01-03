6eac7b8f571d4541672926e25f103e5d53f49525
Murat Ersin <muratersin@yahoo.com>
2024-01-03 18:16:36 +0300

    get api test implemented

--- src/__test__/crud-api.spec.ts
+++ src/__test__/crud-api.spec.ts
@@ -5,10 +5,16 @@ import { makeRequest } from "./test-util";
 
 const BASE_URL = "http://localhost:3000";
 const request = makeRequest(BASE_URL);
+
 test("fetch items", async () => {
   const response = await request("/item", {
     method: "get",
   });
-  console.log(response);
-  assert.strictEqual(response?.data?.length, 1);
+
+  assert.strictEqual(response?.data?.items.length, 1);
+  assert.strictEqual(response?.data?.items[0]?.id, 1);
+  assert.strictEqual(response?.data?.items[0]?.name, "item 1");
+  assert.strictEqual(response?.data?.items[0]?.size, 12);
+  assert.strictEqual(response?.data?.items[0]?.active, true);
+  assert.strictEqual(response?.data?.items[0]?.meta?.material, "wood");
 });
