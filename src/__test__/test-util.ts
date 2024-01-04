f2fd5fc1b3047faef310d8125fb9aa15f2de70d6
Murat Ersin <muratersin@yahoo.com>
2024-01-04 23:03:44 +0300

    mock api delete and edit test added

--- src/__test__/test-util.ts
+++ src/__test__/test-util.ts
@@ -2,7 +2,7 @@ export const makeRequest =
   (baseUrl: string) => (path: string, options: RequestInit | undefined) =>
     new Promise((resolve: (res: any) => void) => {
       fetch(`${baseUrl}/${path}`, options)
-        .then((res) => res.json())
+        .then(async (res) => await res.json())
         .then(resolve)
         .catch(resolve);
     });
